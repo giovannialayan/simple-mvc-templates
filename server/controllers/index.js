6c5ca9e9978b7e149f841945beb7ae021d669314
Giovanni Alayan (RIT Student) <gna7400@rit.edu>
2022-02-28 14:36:15 -0500

    handlebars class example

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,29 +1,46 @@
 // There is no reason for the name here except as an
 // example of how to set something for the POST
-let name = 'unknown';
+let name = [];
 
 const hostIndex = (req, res) => {
-
+  return res.render('index', {
+    title: 'home',
+    pageName: 'home page',
+    username: name,
+  });
 };
 
 const hostPage1 = (req, res) => {
-
+  return res.render('page1', {
+    title: 'page 1',
+    names: ['1', '2', 'three'],
+  });
 };
 
 const hostPage2 = (req, res) => {
-
+  return res.render('page2');
 };
 
 const getName = (req, res) => {
-
+  return res.json({name});
 };
 
 const setName = (req, res) => {
+  if(!req.body.firstname || !req.body.lastname) {
+    return res.status(400).json({
+      error: 'firstname and last name are both required',
+      id: 'setNameMissingParams',
+    });
+  }
 
+  name = `${req.body.firstname} ${req.body.lastname}`;
+  return res.status(200).json({name});
 };
 
 const notFound = (req, res) => {
-
+  return res.render('notFound', {
+    page: req.url,
+  })
 };
 
 module.exports = {
